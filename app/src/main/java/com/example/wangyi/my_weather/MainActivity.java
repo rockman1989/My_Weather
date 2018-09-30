68ca4575ff72a2374ce60cec5cbbb14af7c864a2
rockman1989 <626318323@qq.com>
2018-09-30 17:32:04 +0800

    首次提交

---
+++ app/src/main/java/com/example/wangyi/my_weather/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.wangyi.my_weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wangyi on 2018/9/28.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
