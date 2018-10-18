f699b55be998add437c765a463fd19868afa20e2
rockman1989 <626318323@qq.com>
2018-10-18 19:16:57 +0800

    1.添加城市选择按钮动作 2.添加城市数据库

---
+++ app/src/main/java/com/example/wangyi/SelectCity.java
@@ -0,0 +1,42 @@
+package com.example.wangyi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.wangyi.my_weather.R;
+
+/**
+ * Created by wangyi on 2018/10/18.
+ */
+public class SelectCity extends Activity implements View.OnClickListener {
+
+    private ImageView mBackBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.select_city);
+
+        mBackBtn = (ImageView)findViewById(R.id.title_back);
+        mBackBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.title_back:
+                Intent i = new Intent();
+                i.putExtra("cityCode", "101160101");
+                setResult(RESULT_OK, i);
+                finish();
+                break;
+            default:
+                break;
+
+        }
+    }
+}
