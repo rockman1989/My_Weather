d06cc005c4a6aea9ccd3a31dfe3352d3753caca9
rockman1989 <626318323@qq.com>
2018-10-26 18:40:54 +0800

    添加城市列表，设计点击效果

--- app/src/main/java/com/example/wangyi/SelectCity.java
+++ app/src/main/java/com/example/wangyi/SelectCity.java
@@ -4,9 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 
+import com.example.wangyi.app.MyApplication;
+import com.example.wangyi.bean.City;
 import com.example.wangyi.my_weather.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by wangyi on 2018/10/18.
@@ -23,6 +30,40 @@ public class SelectCity extends Activity implements View.OnClickListener {
 
         mBackBtn = (ImageView)findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
+
+
+        //获得CityList
+        ArrayList<City> myCityList = (ArrayList<City>) MyApplication.getInstance().getCityList();
+        //获得myCityList的长度
+        int City_length = myCityList.size();
+        //存城市名字的数组
+        String[] CityName = new String[City_length];
+        //存城市编码的数组
+        final String[] CityNum = new String[City_length];
+        //给2个城市数组赋值
+        for(int i = 0; i < City_length; i ++){
+            CityName[i] = myCityList.get(i).getCity();
+            CityNum[i] = myCityList.get(i).getNumber();
+        }
+
+        //初始化listview
+        ListView mlistView = (ListView)findViewById(R.id.list_view);
+        //初始化适配器
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SelectCity.this, android.R.layout.simple_list_item_1,CityName);
+
+        mlistView.setAdapter(adapter);
+        //设置点击效果
+        mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(SelectCity.this, CityNum[position], Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent();
+                i.putExtra("cityCode", CityNum[position]);
+                setResult(RESULT_OK, i);
+                finish();
+            }
+        });
     }
 
     @Override
